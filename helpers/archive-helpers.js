611f67925356b415eee52762fa1416d4e2521438
Hack Reactor Student <student@hackreactor.com>
2017-07-26 20:56:12 -0700

    rendering initial html and half of archive helper functions complete

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -26,16 +26,45 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  //somehow read this text file, which is located in paths.list
+  fs.readFile(this.paths.list, 'utf-8', function(err, data) { //data returns as JS string with utf-8
+    if (err) {
+      throw err;
+    } else {
+      // console.log('banana');
+      callback(data.split('\n')); //split string on endlines
+    }
+  });
+  
 };
 
 exports.isUrlInList = function(url, callback) {
+  //going to call readListOfUrls
+  //check if URL passed in is inside paths.list
+  //invoke callback with true or false
+  this.readListOfUrls(function(data) { //this callback gets called on line 35 with ONE parameter, being data
+    if (data.includes(url)) { 
+      callback(true); //if it exists, callback will be isUrlArchived
+    } else {
+      callback(false); //if it doesn't exist, callback will be addUrlToList
+    }
+  });
+
+
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.writeFile(this.paths.list, url); //does not overwrite entire file, simply adds whatever is in second parameter to file
+  callback();
 };
 
 exports.isUrlArchived = function(url, callback) {
+  //check if webpage has been downloaded for given URL
+    //do this by checking if a folder web page html exists in paths.archivedSites
+      
 };
 
 exports.downloadUrls = function(urls) {
+  //if isUrlArchived returns false
+    //downloadUrl from online
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,12 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  fs.readFile('/Users/student/code/hrsf79-web-historian/web/public/index.html', 'utf-8', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data); //write actually writes response to body
+    res.end(); //response just lets server know this is end of response
+  });
 };
